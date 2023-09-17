e2d4ad5810bc9e1f989192ceb0ef2182578b640b
Sepus Noordmans <8858408+seppiee@users.noreply.github.com>
2023-09-17 22:20:32 +0200

    Remove empty strings in database configs

--- config/production/database.ts
+++ config/production/database.ts
@@ -4,13 +4,10 @@ export default ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_PROD_NAME'),
-      user: env('DATABASE_PROD_USERNAME', ''),
-      password: env('DATABASE_PROD_PASSWORD', ''),
+      database: env('DATABASE_PROD_NAME', 'nsfs-db-production'),
+      user: env('DATABASE_PROD_USERNAME'),
+      password: env('DATABASE_PROD_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
-
-
-console.log('Database Name:', env('DATABASE_PROD_NAME'));
