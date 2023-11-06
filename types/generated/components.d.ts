a858cf0402d30147b82a1c3e7c10725a1fbfd371
Sepus Noordmans <8858408+seppiee@users.noreply.github.com>
2023-11-06 12:14:25 +0100

    new fields on special tickets

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -215,11 +215,13 @@ export interface TicketComponentsBasicTickets extends Schema.Component {
   info: {
     displayName: 'Basic Tickets';
     icon: 'bulletList';
+    description: '';
   };
   attributes: {
     name: Attribute.String;
     price: Attribute.Integer;
     hex_color: Attribute.String;
+    extra_info: Attribute.String;
   };
 }
 
@@ -246,6 +248,8 @@ export interface TicketComponentsSpecialTickets extends Schema.Component {
     button_txt: Attribute.String;
     description: Attribute.Text;
     perks: Attribute.Component<'ticket-components.special-perks', true>;
+    price: Attribute.RichText;
+    url: Attribute.String;
   };
 }
 
